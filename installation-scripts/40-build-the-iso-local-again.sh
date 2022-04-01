491d6de4c7b866ec2df8731e2b68bb816f887c64
Vincenzo Caselli <vincenzo.caselli@rcp-vision.com>
2022-04-01 23:24:32 +0200

    Archiso bump

--- installation-scripts/40-build-the-iso-local-again.sh
+++ installation-scripts/40-build-the-iso-local-again.sh
@@ -66,7 +66,7 @@ echo
 	isoLabel='x4os-v22.04.02-x86_64.iso'
 
 	# setting of the general parameters
-	archisoRequiredVersion="archiso 61-1"
+	archisoRequiredVersion="archiso 62-1"
 	buildFolder=$HOME"/arcolinuxb-build"
 	outFolder=$HOME"/ArcoLinuxB-Out"
 	archisoVersion=$(sudo pacman -Q archiso)
